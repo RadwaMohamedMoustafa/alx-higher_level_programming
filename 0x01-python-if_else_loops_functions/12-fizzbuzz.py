3668006e8f75246418dff731c7162420607ffbda
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-06-06 13:56:04 -0700

    new

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(0, 101):
-        msg = " "
-        if i % 3 == 0:
-            msg = "Fizz"
-        if i % 5 == 0:
-            msg = "Buzz"
         if i % 15 == 0:
             msg = "FizzBuzz"
+        if i % 5 == 0:
+            msg = "Buzz"
+        if i % 3 == 0:
+            msg = "Fizz"
         else:
             msg = i
             print(msg, end=' ')
