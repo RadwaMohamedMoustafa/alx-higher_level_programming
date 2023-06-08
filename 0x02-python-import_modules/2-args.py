3c263824617c9b4577ff6c185985300701401008
RadwaMohamedMoustafa <radwa.feshir@gmail.com>
2023-06-08 09:51:32 -0700

    Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    arg = len(sys.argv) - 1
+
+    if arg == 0:
+        print("0 arguments.")
+
+    elif arg == 1:
+        print("1 argument:")
+
+    else:
+        print("{:d} arguments:".format(arg))
+
+
+    for i in range(arg):
+        print("{:d}: {:s}".format(i + 1 , sys.argv[i + 1]))
