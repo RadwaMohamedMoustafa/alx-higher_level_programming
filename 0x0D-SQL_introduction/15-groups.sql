cde0dc2a6a27eeab38e6543a4463a964e13a677a
Radwa <Radwa.feshir@gmail.com>
2023-08-15 23:49:41 +0000

    Ist

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Number By Score
+SELECT `score`, COUNT(`score`) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- No Name
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
