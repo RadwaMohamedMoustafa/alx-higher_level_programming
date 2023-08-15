b934dcaf170537e049a9064163b9c680a5f60dd5
Radwa <Radwa.feshir@gmail.com>
2023-08-15 23:52:00 +0000

    Ist

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Full Creation
+CREATE TABLE If Not EXISTS `second_table` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(256) NOT NULL,
+  `score` INT NOT NULL,
+    PRIMARY KEY (`id`)
+);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
